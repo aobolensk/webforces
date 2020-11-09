a6dbc3557a1ad3e9df42103b195c16a92796ce7d
Arseniy Obolenskiy <gooddoog@student.su>
2020-11-09 18:54:26 +0300

    [Backend] API: Use Django REST framework and djoser

--- webforces/api/urls.py
+++ webforces/api/urls.py
@@ -1,7 +1,9 @@
+import djoser.urls.authtoken
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('stats', views.stats)
+    path('get_token/', djoser.urls.authtoken.views.TokenCreateView.as_view()),
+    path('stats/', views.StatsView.as_view()),
 ]
