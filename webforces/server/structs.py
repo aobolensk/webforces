f54b422935c0c3a8adda9d625aa05d21dfaa3c1a
megarungle <megarungle@gmail.com>
2020-11-21 19:25:28 +0300

    Refactor structs

--- webforces/server/structs.py
+++ webforces/server/structs.py
@@ -1,37 +1,32 @@
 from dataclasses import dataclass, field
 from typing import List
 
-
 @dataclass
 class User:
-    user_id: int
     login: str = ''
     first_name: str = ''
     second_name: str = ''
     middle_name: str = ''
-    algs_id: List[int] = field(default_factory=list)
+    algs_id: list() = field(default_factory=list)
 
 
 @dataclass
 class Algorithm:
-    alg_id: int
     title: str = ''
-    author_id: int = 0
+    author_id: 'typing.Any' = object()
     source: str = ''
-    tests_id: List[int] = field(default_factory=list)
+    tests_id: list() = field(default_factory=list)
     lang_id: int = 0
 
 
 @dataclass
 class Test:
-    test_id: int
     title: str = ''
     source: str = ''
 
 
 @dataclass
 class Task:
-    task_id: int
-    alg_id: int
+    alg_id: 'typing.Any' = object()
     status: int = 0
     message: str = ''
