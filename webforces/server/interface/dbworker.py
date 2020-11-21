24132de13712dde9bf5a58b854df9c875b04f724
megarungle <megarungle@gmail.com>
2020-11-21 17:31:28 +0300

    Implement getUser and addUser

--- webforces/server/interface/dbworker.py
+++ webforces/server/interface/dbworker.py
@@ -9,3 +9,15 @@ class DBWorker(abc.ABC):
     @abc.abstractmethod
     def disconnect(self) -> int:
         pass
+
+    @abc.abstractmethod
+    def getUserByID(self, id) -> dict():
+        pass
+
+    @abc.abstractmethod
+    def getUserByLogin(self, login) -> dict():
+        pass
+
+    @abc.abstractmethod
+    def addUser(self, user_d) -> str:
+        pass
--- webforces/server/mongodbworker.py
+++ webforces/server/mongodbworker.py
@@ -2,6 +2,7 @@ import sys
 sys.path.append('../')
 sys.path.append('../../')
 import pymongo
+from structs import User
 from loguru import logger
 from webforces.server.interface import dbworker
 from webforces.settings import MONGODB_PROPERTIES
@@ -36,6 +37,57 @@ class MongoDBWorker(dbworker.DBWorker):
     def disconnect(self) -> int:
         return 0
 
+    def getUserByID(self, id) -> dict():
+        try:
+            # <PLACEHOLDER>
+            users_collection = self.db["users"]
+            user_d = users_collection.find_one({"_id": id})
+            if user_d is None:
+                logger.error("This user is not exist")
+                return 2
+            # </PLACEHOLDER>
+        except Exception as e:
+            logger.error(f"MongoDBWorker connection failed: {e}")
+            return 1
+        logger.debug("User was found")
+        return user_d
+
+    def getUserByLogin(self, login) -> dict():
+        try:
+            # <PLACEHOLDER>
+            users_collection = self.db["users"]
+            user_d = users_collection.find_one({"login": login})
+            if user_d is None:
+                logger.error("This user is not exist")
+                return 2
+            # </PLACEHOLDER>
+        except Exception as e:
+            logger.error(f"MongoDBWorker connection failed: {e}")
+            return 1
+        logger.debug("User was found")
+        return user_d
+
+    def addUser(self, user_d) -> str:
+        try:
+            # <PLACEHOLDER>
+            users_collection = self.db["users"]
+            if users_collection.find_one({"login": user_d["login"]}) is not None:
+                logger.error("This login is already taken")
+                return 2
+            id = users_collection.insert_one(user_d).inserted_id
+            # </PLACEHOLDER>
+        except Exception as e:
+            logger.error(f"MongoDBWorker connection failed: {e}")
+            return 1
+        logger.debug("New user was successfully added")
+        return id
 
 if __name__ == "__main__":
     MDB = MongoDBWorker()
+    user = User(1, "login1", "fn", "sn", "mn", [0])
+    user_d = user.__dict__
+    #MDB.addUser(user_d)
+    user_d2 = MDB.getUserByLogin("login1")
+    print(user_d2["_id"])
+    user_d3 = MDB.getUserByID(user_d2["_id"])
+    print(user_d2 == user_d3)
