485af8c05c652102f8041d83773e9b7b94d4c0f5
Arseniy Obolenskiy <gooddoog@student.su>
2020-11-22 19:34:04 +0300

    [Frontend] Add sign up page

--- webforces/urls.py
+++ webforces/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/sign_up/', views.sign_up),
     path('api/', include('webforces.api.urls')),
 ]
