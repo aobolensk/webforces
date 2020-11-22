f06c64dec01839db358fda3cc4dcee0426076889
Arseniy Obolenskiy <gooddoog@student.su>
2020-11-22 22:55:36 +0300

    [Backend] Add API endpoints for users

--- webforces/api/urls.py
+++ webforces/api/urls.py
@@ -6,4 +6,6 @@ from . import views
 urlpatterns = [
     path('get_token/', djoser.urls.authtoken.views.TokenCreateView.as_view()),
     path('stats/', views.StatsView.as_view()),
+    path('users/', views.UsersView.as_view()),
+    path('users/<int:pk>/', views.UserView.as_view()),
 ]
--- webforces/api/views.py
+++ webforces/api/views.py
@@ -1,6 +1,9 @@
+from django.http.response import Http404
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from webforces.server.core import Core
+from webforces.server.structs import DBStatus
 
 
 class StatsView(APIView):
@@ -11,3 +14,36 @@ class StatsView(APIView):
             "name": "webforces",
         }
         return Response(stats)
+
+
+class UsersView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        users = []
+
+        data = {
+            "count": len(users),
+        }
+        return Response(data)
+
+
+class UserView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request, pk):
+        core = Core()
+        status, user = core.db.getUserByID(pk)
+
+        if status != DBStatus.s_ok:
+            raise Http404("User does not exist")
+
+        data = {
+            "id": user.user_id,
+            "login": user.login,
+            "first_name": user.first_name,
+            "second_name": user.second_name,
+            "middle_name": user.middle_name,
+        }
+
+        return Response(data)
