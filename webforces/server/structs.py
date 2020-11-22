fdf9a7bbee827a822414c5fc25e32ca8abf72e5e
megarungle <megarungle@gmail.com>
2020-11-22 19:27:57 +0300

    Refactor structs and methods. Add tests

--- webforces/server/structs.py
+++ webforces/server/structs.py
@@ -1,32 +1,52 @@
 from dataclasses import dataclass, field
 from typing import List
+from enum import Enum
+
+
+class DBStatus(Enum):
+    s_ok = 0
+    s_connection_error = -1
+    s_data_issue = -2
+
 
 @dataclass
 class User:
+    user_id: int
     login: str = ''
     first_name: str = ''
     second_name: str = ''
     middle_name: str = ''
-    algs_id: list() = field(default_factory=list)
+    algs_id: List[int] = field(default_factory=list)
+
+    @classmethod
+    def fromDict(cls, dict):
+        return cls(
+            dict["user_id"], dict["login"],
+            dict["first_name"], dict["second_name"], dict["middle_name"],
+            dict["algs_id"]
+        )
 
 
 @dataclass
 class Algorithm:
+    alg_id: int
     title: str = ''
-    author_id: 'typing.Any' = object()
+    author_id: int = 0
     source: str = ''
-    tests_id: list() = field(default_factory=list)
+    tests_id: List[int] = field(default_factory=list)
     lang_id: int = 0
 
 
 @dataclass
 class Test:
+    test_id: int
     title: str = ''
     source: str = ''
 
 
 @dataclass
 class Task:
-    alg_id: 'typing.Any' = object()
+    task_id: int
+    alg_id: int
     status: int = 0
     message: str = ''
--- webforces/server/tests.py
+++ webforces/server/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from webforces.server.core import Core
+from webforces.server.structs import DBStatus, User
 
 
 class CoreTest(TestCase):
@@ -9,3 +10,67 @@ class CoreTest(TestCase):
     def test_core_is_proper_singletone(self):
         core2 = Core()
         self.assertIs(core2, self.core)
+
+
+class DBTest(TestCase):
+    def setUp(self):
+        self.core = Core()
+
+    def tearDown(self):
+        self.core.db.dropAll()
+
+    def test_can_add_diff_users(self):
+        user1 = User(0, "LOGIN_USER1", "FN_USER1", "SN_USER1", "MN_USER1", [])
+        user2 = User(0, "LOGIN_USER2", "FN_USER2", "SN_USER2", "MN_USER2", [])
+        status1, user1 = self.core.db.addUser(user1)
+        status2, user2 = self.core.db.addUser(user2)
+        self.assertEqual(status1, DBStatus.s_ok)
+        self.assertEqual(status2, DBStatus.s_ok)
+        self.assertEqual(user1.user_id, 1)
+        self.assertEqual(user2.user_id, 2)
+        self.assertEqual(self.core.db._getNextID("1u"), 1)
+        self.assertEqual(self.core.db._getNextID("2u"), 1)
+
+    def test_cant_add_iden_users(self):
+        user1 = User(0, "LOGIN_USER1", "FN_USER1", "SN_USER1", "MN_USER1", [])
+        user2 = User(0, "LOGIN_USER1", "FN_USER2", "SN_USER2", "MN_USER2", [])
+        status1, user1 = self.core.db.addUser(user1)
+        status2, user2 = self.core.db.addUser(user2)
+        self.assertEqual(status1, DBStatus.s_ok)
+        self.assertEqual(status2, DBStatus.s_data_issue)
+        self.assertEqual(user1.user_id, 1)
+        self.assertEqual(user2.user_id, -100)
+        self.assertEqual(self.core.db._getNextID("1u"), 1)
+        self.assertEqual(self.core.db._getNextID("2u"), DBStatus.s_data_issue)
+
+    def test_can_get_user_by_correct_id(self):
+        user1 = User(0, "LOGIN_USER1", "FN_USER1", "SN_USER1", "MN_USER1", [])
+        status1, user1 = self.core.db.addUser(user1)
+        status2, user2 = self.core.db.getUserByID(user1.user_id)
+        self.assertEqual(status1, DBStatus.s_ok)
+        self.assertEqual(status2, DBStatus.s_ok)
+        self.assertEqual(user1, user2)
+
+    def test_cant_get_user_by_incorrect_id(self):
+        user1 = User(0, "LOGIN_USER1", "FN_USER1", "SN_USER1", "MN_USER1", [])
+        status1, user1 = self.core.db.addUser(user1)
+        status2, user2 = self.core.db.getUserByID(user1.user_id + 1)
+        self.assertEqual(status1, DBStatus.s_ok)
+        self.assertEqual(status2, DBStatus.s_data_issue)
+        self.assertEqual(user2.user_id, -100)
+
+    def test_can_get_user_by_correct_login(self):
+        user1 = User(0, "LOGIN_USER1", "FN_USER1", "SN_USER1", "MN_USER1", [])
+        status1, user1 = self.core.db.addUser(user1)
+        status2, user2 = self.core.db.getUserByLogin(user1.login)
+        self.assertEqual(status1, DBStatus.s_ok)
+        self.assertEqual(status2, DBStatus.s_ok)
+        self.assertEqual(user1, user2)
+
+    def test_cant_get_user_by_incorrect_login(self):
+        user1 = User(0, "LOGIN_USER1", "FN_USER1", "SN_USER1", "MN_USER1", [])
+        status1, user1 = self.core.db.addUser(user1)
+        status2, user2 = self.core.db.getUserByLogin(user1.login + "error")
+        self.assertEqual(status1, DBStatus.s_ok)
+        self.assertEqual(status2, DBStatus.s_data_issue)
+        self.assertEqual(user2.user_id, -100)
