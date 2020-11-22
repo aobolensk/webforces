0bed5e8d48e0a0f703635b41c9faa6d791893800
Sergey Konnov <sergeyzorro007@gmail.com>
2020-11-23 01:24:28 +0300

    initiate main page

--- webforces/urls.py
+++ webforces/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', views.MainPageView.as_view()),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/sign_up/', views.sign_up),
--- webforces/views.py
+++ webforces/views.py
@@ -1,7 +1,27 @@
+from dataclasses import dataclass
+
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+from django.views.generic.base import TemplateView
 
+@dataclass
+class Href:
+    url: str = ''
+    description: str = ''
+
+class MainPageView(TemplateView):
+    template_name = "base.html"
+
+    _index = [
+        Href("/accounts/sign_in/", "sign in"),
+        Href("/accounts/sign_up/", "sign up"),
+    ]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['index'] = self._index
+        return context
 
 def sign_up(request):
     if request.method == 'POST':
