a45d26bcdec1a6c144d409b43d598a0c883021ec
Arseniy Obolenskiy <gooddoog@student.su>
2020-12-01 02:17:58 +0300

    [Backend] Add REST API tests for guests, users and superusers

--- webforces/api/tests.py
+++ webforces/api/tests.py
@@ -3,17 +3,72 @@ from django.test import Client, TestCase
 from webforces.server.core import Core
 
 
-class RestApiTest(TestCase):
+class RestApiSuperUserTest(TestCase):
+    """Test REST API with user permission level"""
+
+    username = 'testsuperuser'
+    password = '123'
+
     def setUp(self):
         self.core = Core()
-        user = User.objects.create(username='testuser')
-        user.set_password('123')
+        user = User.objects.create(username=self.username)
+        user.set_password(self.password)
+        user.is_staff = True
+        user.is_admin = True
+        user.is_superuser = True
         user.save()
         self.client = Client()
-        self.client.login(username='testuser', password='123')
+        self.client.login(username=self.username, password=self.password)
 
     def testStatsEndpoint(self):
         response = self.client.get('/api/stats/')
         self.assertEqual(response.status_code, 200)
         stats = response.json()
         self.assertEqual(stats["name"], "webforces")
+
+    def testGetTokenEndpoint(self):
+        response = self.client.post('/api/get_token/', {
+            "username": self.username,
+            "password": self.password
+        })
+        jsn = response.json()
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue("auth_token" in jsn.keys())
+
+
+class RestApiRegularUserTest(RestApiSuperUserTest):
+    """Test REST API with user permission level"""
+
+    username = 'testuser'
+    password = '123'
+
+    def setUp(self):
+        self.core = Core()
+        user = User.objects.create(username=self.username)
+        user.set_password(self.password)
+        user.save()
+        self.client = Client()
+        self.client.login(username=self.username, password=self.password)
+
+
+class RestApiGuestTest(RestApiRegularUserTest):
+    """Test REST API with guest permission level"""
+
+    username = None
+    password = None
+
+    def setUp(self):
+        self.core = Core()
+        self.client = Client()
+
+    def testStatsEndpoint(self):
+        response = self.client.get('/api/stats/')
+        self.assertEqual(response.status_code, 403)
+
+    def testUsersEndpoint(self):
+        response = self.client.get('/api/users/')
+        self.assertEqual(response.status_code, 403)
+
+    def testGetTokenEndpoint(self):
+        response = self.client.post('/api/get_token/')
+        self.assertEqual(response.status_code, 400)
