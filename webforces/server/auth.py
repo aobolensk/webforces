12605076d3137060951de1bc3e80d16be3b5e049
Sergey Konnov <sergeyzorro007@gmail.com>
2020-12-07 00:37:44 +0300

    create MongoDB user instances on login and signup
    close #53

--- webforces/server/auth.py
+++ webforces/server/auth.py
@@ -1,9 +1,24 @@
 from loguru import logger
+from webforces.server.structs import DBStatus, User
 
 
 class Auth:
     def authenticate(self, login: str, password: str) -> bool:
+        from webforces.server.core import Core
+        core = Core()
+        status, user = core.db.getUserByLogin(login)
+        if status != DBStatus.s_ok:
+            user = User(0, login, "", "", "", [])
+            core.db.addUser(user)
+            logger.debug(f"Add new user to mongodb: {user}")
         logger.debug(f"Logging in user: '{login}', password: {password}'")
 
     def register(self, login: str, password: str) -> bool:
+        from webforces.server.core import Core
+        core = Core()
+        status, user = core.db.getUserByLogin(login)
+        if status != DBStatus.s_ok:
+            user = User(0, login, "", "", "", [])
+            core.db.addUser(user)
+            logger.debug(f"Add new user to mongodb: {user}")
         logger.debug(f"Signing up user: '{login}', password: {password}'")
