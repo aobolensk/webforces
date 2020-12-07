8136414de157246fa50c1cf513a02e56d03b9174
Arseniy Obolenskiy <gooddoog@student.su>
2020-12-07 19:26:08 +0300

    [Backend] Create MongoDB user instances on /api/get_token POST request

--- webforces/api/urls.py
+++ webforces/api/urls.py
@@ -1,11 +1,10 @@
-import djoser.urls.authtoken
 from django.urls import path
 
 from . import index, views
 
 urlpatterns = [
     path('', index.APIIndexView.as_view()),
-    path('get_token/', djoser.urls.authtoken.views.TokenCreateView.as_view()),
+    path('get_token/', views.GetTokenView.as_view()),
     path('stats/', views.StatsView.as_view()),
     path('users/', views.UsersView.as_view()),
     path('users/<int:pk>/', views.UserView.as_view()),
