28c399c9696ae0380f2113c8b0545d659fa1448e
Arseniy Obolenskiy <gooddoog@student.su>
2020-12-14 00:10:52 +0300

    [Backend] Return actual information in '/api/users' and add more REST API tests

--- webforces/api/tests.py
+++ webforces/api/tests.py
@@ -1,6 +1,7 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User as DjangoUser
 from django.test import Client, TestCase
 from webforces.server.core import Core
+from webforces.server.structs import User
 
 
 class RestApiSuperUserTest(TestCase):
@@ -10,9 +11,9 @@ class RestApiSuperUserTest(TestCase):
     password = '123'
 
     def setUp(self):
-        self.core = Core()
+        self.core = Core(validation=True)
         self.core.db._populateIds()
-        user = User.objects.create(username=self.username)
+        user = DjangoUser.objects.create(username=self.username)
         user.set_password(self.password)
         user.is_staff = True
         user.is_admin = True
@@ -39,6 +40,41 @@ class RestApiSuperUserTest(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue("auth_token" in jsn.keys())
 
+    def testGetUsersEndpointEmpty(self):
+        response = self.client.get('/api/users/')
+        self.assertEqual(response.status_code, 200)
+        stats = response.json()
+        self.assertEqual(stats["count"], 0)
+
+    def testGetUsersEndpointWithAddedUser(self):
+        self.core.db.addUser(User(0, "LOGIN_USER1", "FN_USER1", "SN_USER1", "MN_USER1", []))
+        response = self.client.get('/api/users/')
+        self.assertEqual(response.status_code, 200)
+        stats = response.json()
+        self.assertEqual(stats["count"], 1)
+
+    def testGetUserByIdEndpoint(self):
+        self.core.db.addUser(User(0, "LOGIN_USER1", "FN_USER1", "SN_USER1", "MN_USER1", []))
+        response = self.client.get('/api/users/1/')
+        self.assertEqual(response.status_code, 200)
+        stats = response.json()
+        self.assertEqual(stats["id"], 1)
+        self.assertEqual(stats["login"], "LOGIN_USER1")
+        self.assertEqual(stats["first_name"], "FN_USER1")
+        self.assertEqual(stats["second_name"], "SN_USER1")
+        self.assertEqual(stats["middle_name"], "MN_USER1")
+
+    def testGetUserByLoginEndpoint(self):
+        status, user = self.core.db.addUser(User(0, "LOGIN_USER1", "FN_USER1", "SN_USER1", "MN_USER1", []))
+        response = self.client.get('/api/users/LOGIN_USER1/')
+        self.assertEqual(response.status_code, 200)
+        stats = response.json()
+        self.assertEqual(stats["id"], 1)
+        self.assertEqual(stats["login"], "LOGIN_USER1")
+        self.assertEqual(stats["first_name"], "FN_USER1")
+        self.assertEqual(stats["second_name"], "SN_USER1")
+        self.assertEqual(stats["middle_name"], "MN_USER1")
+
 
 class RestApiRegularUserTest(RestApiSuperUserTest):
     """Test REST API with user permission level"""
@@ -49,7 +85,7 @@ class RestApiRegularUserTest(RestApiSuperUserTest):
     def setUp(self):
         self.core = Core()
         self.core.db._populateIds()
-        user = User.objects.create(username=self.username)
+        user = DjangoUser.objects.create(username=self.username)
         user.set_password(self.password)
         user.save()
         self.client = Client()
@@ -84,3 +120,22 @@ class RestApiGuestTest(RestApiRegularUserTest):
     def testGetTokenEndpoint(self):
         response = self.client.post('/api/get_token/')
         self.assertEqual(response.status_code, 400)
+
+    def testGetUsersEndpointEmpty(self):
+        response = self.client.get('/api/users/')
+        self.assertEqual(response.status_code, 403)
+
+    def testGetUsersEndpointWithAddedUser(self):
+        self.core.db.addUser(User(0, "LOGIN_USER1", "FN_USER1", "SN_USER1", "MN_USER1", []))
+        response = self.client.get('/api/users/')
+        self.assertEqual(response.status_code, 403)
+
+    def testGetUserByIdEndpoint(self):
+        status, user = self.core.db.addUser(User(0, "LOGIN_USER1", "FN_USER1", "SN_USER1", "MN_USER1", []))
+        response = self.client.get('/api/users/1/')
+        self.assertEqual(response.status_code, 403)
+
+    def testGetUserByLoginEndpoint(self):
+        status, user = self.core.db.addUser(User(0, "LOGIN_USER1", "FN_USER1", "SN_USER1", "MN_USER1", []))
+        response = self.client.get('/api/users/LOGIN_USER1/')
+        self.assertEqual(response.status_code, 403)
