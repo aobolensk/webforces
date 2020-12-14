4a4c54d7b5275d71f38f2674c0f00691507be502
Arseniy Obolenskiy <gooddoog@student.su>
2020-12-14 03:44:37 +0300

    [Frontend] Update API index, add info about available HTTP request methods

--- webforces/api/index.py
+++ webforces/api/index.py
@@ -1,4 +1,5 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
 
 from webforces.views import MainPageView
 
@@ -8,14 +9,17 @@ class Endpoint:
     real_url: str = ''
     show_url: str = ''
     description: str = ''
+    methods: List[str] = field(default_factory=list)
 
 
 class APIIndexView(MainPageView):
     template_name = "api_index.html"
 
     _index = [
-        Endpoint("/api/stats", "/api/stats", "Get statistics"),
-        Endpoint("/api/get_token", "/api/get_token", "Get API token"),
+        Endpoint("/api/stats", "/api/stats", "Get statistics", ["GET"]),
+        Endpoint("/api/users/1", "/api/stats/<int:id>", "Get user by ID", ["GET", "POST"]),
+        Endpoint("/api/users/admin/", "/api/stats/<str:username>", "Get user by username", ["GET", "POST"]),
+        Endpoint("/api/get_token", "/api/get_token", "Get API token", ["POST"]),
     ]
 
     def get_context_data(self, **kwargs):
