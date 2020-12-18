f869cc52385532ccf0a2d7be8ec24fcf9196975c
Savkin Yuriy <catasd@yandex.ru>
2020-12-18 07:58:50 +0300

    fixes for ci

--- webforces/server/selenium_tests.py
+++ webforces/server/selenium_tests.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium.webdriver.remote.webdriver import WebDriver
 
+
 @pytest.mark.web_test
 def test_can_get_start_page(selenium: WebDriver):
     selenium.get("http://127.0.0.1:8000/")
