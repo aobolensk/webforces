2bfb1a74c15836bb3eed91f035c782581dfeaac4
Sergey Konnov <sergeyzorro007@gmail.com>
2020-12-20 01:18:08 +0300

    add cost for algs pages

--- webforces/forms.py
+++ webforces/forms.py
@@ -3,8 +3,11 @@ from django import forms
 
 class NewAlgForm(forms.Form):
     title = forms.CharField(max_length=100, label="",
-                            widget=forms.TextInput(attrs={'id': 'NewAlgTitle'}))
+                            widget=forms.TextInput(attrs={'id': 'NewAlgTitle', 'placeholder': 'Title'}))
     description = forms.CharField(label="",
-                                  widget=forms.Textarea(attrs={'id': 'NewAlgDescription'}))
+                                  widget=forms.Textarea(attrs={'id': 'NewAlgDescription',
+                                                               'placeholder': 'Description'}))
+    cost = forms.FloatField(min_value=0, label="",
+                            widget=forms.TextInput(attrs={'id': 'NewAlgCost', 'placeholder': 'Cost'}))
     source = forms.CharField(label="",
-                             widget=forms.Textarea(attrs={'id': 'NewAlgSource'}))
+                             widget=forms.Textarea(attrs={'id': 'NewAlgSource', 'placeholder': 'Source'}))
